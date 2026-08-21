356ce0c8945e90c64981e7d4796b4f4f63ad0e56
NITESH CHAWLA <niteshchawla81@gmail.com>
2026-08-21 21:28:01 +1000

    Add files via upload

---
+++ Scanners.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Scanner Dashboard", page_icon="🔎", layout="wide")
+
+st.title("🔎 Scanner Dashboard")
+
+st.markdown("### These are all my scanners for NSE, US, ASX and Crypto markets")
+
+st.info("Use the sidebar to navigate to individual scanners.")
